5f1a865af22ca044923294e665d085f254b5669c
Dumitru <dumitrumerlusca@gmail.com>
2022-01-19 18:00:36 +0200

    Added meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,18 @@ import React, { Fragment } from "react";
 import Header from "../components/layout/Header/Header";
 import Showcase from "../components/layout/Showcase/Showcase";
 import Projects from "../components/layout/projects/Projects";
+import Head from "next/head";
 
 const index = () => {
 	return (
 		<Fragment>
+			<Head>
+				<title>Merlusca Dumitru, Front End Developer</title>
+				<meta
+					name='description'
+					content='Junior self-taught front end developer personal website and portfolio '
+				/>
+			</Head>
 			<Header />
 			<Showcase />
 			<Projects />
